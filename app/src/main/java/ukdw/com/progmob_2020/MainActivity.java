6867e8a23a053d494543bcea6d79975568e27d86
VinandaKristianti <vinandakristianti347@gmail.com>
2020-09-29 14:06:35 +0700

    Materi : Buat activity help

--- app/src/main/java/ukdw/com/progmob_2020/MainActivity.java
+++ app/src/main/java/ukdw/com/progmob_2020/MainActivity.java
@@ -2,7 +2,12 @@ package ukdw.com.progmob_2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +17,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView txtView = (TextView)findViewById(R.id.mainActivityTextView);
+        //variabel
+        final TextView txtView = (TextView)findViewById(R.id.mainActivityTextView);
+        Button btnTombol = (Button)findViewById(R.id.tombol);
+        final EditText txtNama = (EditText)findViewById(R.id.namePerson);
+        Button btnHelp = (Button)findViewById(R.id.btnHelp);
+
+        //action
         txtView.setText(R.string.text_hello_world);
+        btnTombol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.d("Coba Klik", txtNama.getText().toString());
+                txtView.setText(txtNama.getText().toString());
+            }
+        });
+
+        btnHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HelpActivity.class);
+                Bundle b = new Bundle();
+
+                b.putString("help_string",txtNama.getText().toString());
+                intent.putExtras(b);
+
+                startActivity(intent);
+            }
+        });
     }
 }
