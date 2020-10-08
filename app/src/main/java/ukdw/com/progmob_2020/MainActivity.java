85201adcf2e41fd94291c59980b84e499403f52d
VinandaKristianti <vinandakristianti347@gmail.com>
2020-10-08 22:55:09 +0700

    Tugas 3 : Card View

--- app/src/main/java/ukdw/com/progmob_2020/MainActivity.java
+++ app/src/main/java/ukdw/com/progmob_2020/MainActivity.java
@@ -4,16 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 
-import ukdw.com.progmob_2020.Adapter.MahasiswaRecyclerAdapter;
 import ukdw.com.progmob_2020.Materi3.ListActivity;
 import ukdw.com.progmob_2020.Materi3.RecyclerActivity;
+import ukdw.com.progmob_2020.Materi3.cardviewtest_activity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -88,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
         btnCard.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RecyclerActivity.class);
+                Intent intent = new Intent(MainActivity.this, cardviewtest_activity.class);
                 startActivity(intent);
             }
         });
